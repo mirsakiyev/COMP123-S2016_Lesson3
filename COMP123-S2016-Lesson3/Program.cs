cbbd810740dc1c7560cb6962d638e3fe10e4f452
mirsakiyev <mirsakiyev@yahoo.com>
2016-05-24 14:20:04 -0400

    Updated Person class to include Name property and two constructors

--- COMP123-S2016-Lesson3/Program.cs
+++ COMP123-S2016-Lesson3/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace COMP123_S2016_Lesson3
 {
+    /**
+     * This class is the driver class for our Program
+     * 
+     * @class Program
+     */
     public class Program
     {
+        /**
+         * The main method for our driver class Program
+         *
+         * @method Main
+         * @param {strinf[]} args
+         */
         public static void Main(string[] args)
         {
+            // Create a new instance of the Person class
+            Person person = new Person("Aslan");
+
+            Console.WriteLine(person.Name);
+
+            // Set a value in the Name property of the Person class
+            //person.Name = "Aslan";
+
+            // Get the Name value and print to console
+            Console.WriteLine(person.Name);
+
         }
     }
 }
